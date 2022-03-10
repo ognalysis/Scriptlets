09f0b219fc4802a7e6814bd9752913bca7b40d23
Ognalysis <79722369+ognalysis@users.noreply.github.com>
2022-03-10 09:27:28 -0600

    KB4 related Exchange Powershell
    Simple script that checks for "*@psm.knowbe4.com>" in the MessageID to release simulated phishing emails from quarantine. Checks 1000 pages over the past 6 days, including current day. Has not been tested with large exchange deployments. You may want to tune down the page size if your deployment is significantly large.
    
    Will not work if you have disabled showing psm.knowbe4.com in mail headers.

---
+++ KnowBe4QuarantineRelease.ps1
@@ -0,0 +1,39 @@
+﻿Connect-ExchangeOnline
+
+$daysago = (Get-Date).AddDays(-6).ToString("MM/dd/yyyy")
+
+$Tomorrow = (Get-Date).AddDays(1).ToString("MM/dd/yyyy")
+
+$Quarantine = Get-QuarantineMessage -StartReceivedDate $daysago -EndReceivedDate $Tomorrow -PageSize 1000
+
+<#
+#$Header = Get-QuarantineMessageHeader -Identity $Quarantine[0].Identity
+
+$authorized_IPs = @("23.21.109.197","23.21.109.212")
+
+foreach ($x in $Quarantine){
+    foreach ($ip in $authorized_IPs){
+        $Header = Get-QuarantineMessageHeader -Identity $x.Identity
+        if ($Header.Header -like "*Authentication-Results: spf=* (sender IP is $ip)*") {
+            echo "$($x.MessageId) Matches $($ip)"
+        }
+    }
+}
+#>
+
+$results = @()
+
+foreach ($message in $Quarantine) {
+    if ($message.MessageId.ToString() -like "*@psm.knowbe4.com>"){
+        $results += $message
+    }
+}
+
+foreach ($item in $results) {
+    if ($item.ReleaseStatus -eq "NOTRELEASED"){
+        Get-QuarantineMessage -MessageID $item.MessageId | Release-QuarantineMessage -ReleaseToAll
+        Write-Host "Received: $($item.ReceivedTime) - Message $($item.MessageId) - Subject: $($item.Subject) - Status: Released";
+    }
+}
+
+Disconnect-ExchangeOnline -Confirm:$false
