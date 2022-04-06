500bfc500a9ee98a2cfd27e9744284efaa083615
Ognalysis <79722369+ognalysis@users.noreply.github.com>
2022-04-06 16:07:53 -0500

    Add files via upload

---
+++ get-printerIPFromPortName.ps1
@@ -0,0 +1,6 @@
+﻿$SRV = Read-Host -Prompt "Enter Server Name"
+invoke-command -computername $SRV -credential ${Get-Credential} -scriptBlock {
+    $printerName = Read-Host -Prompt "Enter Printer Name"
+    $printer = Get-Printer -Name $printerName | select PortName
+    Get-PrinterPort -Name $printer.PortName | ft DeviceURL    
+}
