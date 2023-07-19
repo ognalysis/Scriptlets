70aa6c4f3af0381f72696d7714842a98405e27bc
Ognalysis <79722369+ognalysis@users.noreply.github.com>
2023-07-19 16:48:53 -0500

    Update EdgeGiveaway.py
    Fixed rolling IDs, fixed elements obscured by window borders, added scrolling into view for elements.

--- EdgeGiveaway.py
+++ EdgeGiveaway.py
@@ -19,10 +19,19 @@ except:
 #print("Keyword:", keyword)
 
 # Form Info
-em = "test.data@imfrench.co.fr"
-fn = "Francois"
-ln = "Frenchman"
+em = "baguette@eifeltower.co.fr"
+fn = "Pierre"
+ln = "Francois"
 pn = "9185551234"
+
+# Element Names
+Efn = "input_1.3"
+Eln = "input_1.6"
+Eem = "input_2"
+Eem2 = "input_2_2"
+Epn = "input_10"
+Ekw = "input_9"
+Ecb = "input_5.1"
 
 # Browser Setup
 options = webdriver.FirefoxOptions()
@@ -31,34 +40,35 @@ options.set_preference("geo.prompt.testing.allow", False)
 driver = webdriver.Firefox(options=options)
 
 driver.get("https://edgetulsa.com/contests/hawaiitripgiveaway/")
-#time.sleep(3);
+time.sleep(3);
 
 # Fill Form Fields
-first_name = driver.find_element(By.ID, "input_398_1_3")
+first_name = driver.find_element(By.NAME, Efn)
+driver.execute_script('arguments[0].scrollIntoView();', first_name);
 first_name.click();
 first_name.send_keys(fn);
 
-last_name = driver.find_element(By.ID, "input_398_1_6")
+last_name = driver.find_element(By.NAME, Eln)
 last_name.click();
 last_name.send_keys(ln);
 
-email = driver.find_element(By.ID, "input_398_2")
+email = driver.find_element(By.NAME, Eem)
 email.click();
 email.send_keys(em);
 
-email2 = driver.find_element(By.ID, "input_398_2_2")
+email2 = driver.find_element(By.NAME, Eem2)
 email2.click();
 email2.send_keys(em);
 
-phone = driver.find_element(By.ID, "input_398_10")
+phone = driver.find_element(By.NAME, Epn)
 phone.click();
 phone.send_keys(pn);
 
-keyword = driver.find_element(By.ID, "input_398_9")
+keyword = driver.find_element(By.NAME, Ekw)
 keyword.click();
 keyword.send_keys(kw);
 
-checkbox = driver.find_element(By.ID, "input_398_5_1").click()
+checkbox = driver.find_element(By.NAME, Ecb).click()
 
 # Send it
 driver.find_element(By.ID, "gform_submit_button_398").click()
