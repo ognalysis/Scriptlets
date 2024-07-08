ddf873039854a0fb73eded73be76b3553881e403
Ognalysis <79722369+ognalysis@users.noreply.github.com>
2024-07-08 17:32:57 -0500

    Update fwd-4-rev-dns.py

--- Python/fwd-4-rev-dns.py
+++ Python/fwd-4-rev-dns.py
@@ -1,8 +1,11 @@
-# Record Example:
-# olp-216-14-144-0        IN      A       216.14.144.0
+import sys
 
-str = "38.187.145"
-subnet = str.split(".")
+# Record Example:
+# example-111-222-333-0        IN      A       111.222.333.0
+
+#str = "111.222.333."
+str = sys.argv
+subnet = str[1].split(".")
 
 for ip in range(256):
 	print("olp-", subnet[0], "-", subnet[1], "-", subnet[2], "-", ip, "\t\tIN\tA\t", subnet[0], ".", subnet[1], ".", subnet[2], ".", ip, sep='')
