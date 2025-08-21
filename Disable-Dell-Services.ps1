acc2d676178243b5140448c802acad4d89272243
Ognalysis <79722369+ognalysis@users.noreply.github.com>
2025-08-21 16:18:53 -0500

    Add files via upload

---
+++ Disable-Dell-Services.ps1
@@ -0,0 +1,14 @@
+﻿#Requires -RunAsAdministrator
+
+$services = Get-Service | where -Property DisplayName -Match "^Dell*"
+
+Foreach ($s in $services){
+    
+    # Stop Services
+    if ($s -ne $null -and $s.status -eq "Running"){ Stop-Service $s }
+    elseif ($s -eq $null) { Write-Host "Serivce '$s' not found." }
+    else { Write-Host "Service '$s' is already stopped." }
+
+    # Set Services to Manual
+    Set-Service -Name $s.Name -StartupType Manual
+}
